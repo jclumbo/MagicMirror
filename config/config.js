7673483740d5875498bd688eadcd6d6bcd31b960
jclumbo <jclumbo@yahoo.com>
2017-01-23 15:05:05 -0500

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,78 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+var config = {
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
+
+	language: 'en',
+	timeFormat: 12,
+	units: 'imperial',
+
+	modules: [
+		{
+			module: 'alert',
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: 'clock',
+			position: 'top_left'
+		},
+		{
+			module: 'calendar',
+			header: 'US Holidays',
+			position: 'top_left',
+			config: {
+				calendars: [
+					{
+						symbol: 'calendar-check-o ',
+						url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
+					}
+				]
+			}
+		},
+		{
+			module: 'currentweather',
+			position: 'top_right',
+			config: {
+				location: 'Framingham, US',
+				locationID: '4937230',  //ID from http://www.openweathermap.org
+				appid: 'f2c387c775746c80e33242080d522ff5'
+			}
+		},
+		{
+			module: 'weatherforecast',
+			position: 'top_right',
+			header: 'Weather Forecast',
+			config: {
+				location: 'Framingham, US',
+				locationID: '4937230',  //ID from http://www.openweathermap.org
+				appid: 'f2c387c775746c80e33242080d522ff5'
+			}
+		},
+		{
+			module: 'newsfeed',
+			position: 'bottom_bar',
+			config: {
+				feeds: [
+					{
+						title: "New York Times",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== 'undefined') {module.exports = config;}
